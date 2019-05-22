8dfa8bcbbdcca6def2dc26d25eab006759960feb
Halden Lin <halden.lin@gmail.com>
2019-05-21 19:09:21 -0700

    Initial commit.

---
+++ js/src/asp2vl.ts
@@ -0,0 +1,92 @@
+import { TopLevelUnitSpec } from 'vega-lite/src/spec/unit';
+import { doesMatchRegex } from './util';
+
+export type VegaLiteSpecDictionary = { [name: string]: TopLevelUnitSpec };
+
+const VIEW_REGEX_CAPTURE = /view\((.*)\)./;
+const FACT_REGEX = /(\w+)\(([\w\.\/]+)(,([\w\.]+))?(,([\w\.]+))?\)/;
+
+export default function asp2vl(facts: string[]): VegaLiteSpecDictionary {
+  const views = facts.filter(fact => {
+    return doesMatchRegex(fact, VIEW_REGEX_CAPTURE);
+  })
+  .map(fact => {
+    const extract = VIEW_REGEX_CAPTURE.exec(fact);
+    if (extract) {
+      const [_, name] = extract;
+      return name;
+    } else {
+      throw new Error(`Invalid view statement: ${fact}.`);
+    }
+  });
+
+  const result = views.reduce((dict, v) => {
+    dict[v] = asp2vl_view(facts, v);
+    return dict;
+  }, {} as any);
+
+  return result;
+}
+
+function asp2vl_view(facts: string[], view: string): TopLevelUnitSpec {
+  let mark;
+  const encodings: { [enc: string]: any } = {};
+
+  for (const value of facts) {
+    const extract = FACT_REGEX.exec(value);
+    if (!extract) {
+      throw new Error(`Invalid fact: ${value}`);
+    }
+
+    const [_, predicate, viz, __, first, ___, second] = extract;
+
+    if (predicate === 'view') {
+      continue;
+    }
+
+    switch (predicate) {
+      case 'mark':
+        mark = first;
+        break;
+      case 'field':
+      case 'type':
+      case 'channel':
+      case 'scale':
+      case 'bin':
+      case 'aggregate':
+      case 'stack':
+        if (!encodings[first]) {
+          encodings[first] = {};
+        }
+
+        encodings[first][predicate] = second;
+    }
+  }
+
+  const encoding: { [channel: string]: any } = {};
+
+  for (const e of Object.keys(encodings)) {
+    const enc = encodings[e];
+
+    const scale = {
+      ...(enc.scale === 'log' ? { type: 'log' } : {}),
+      ...(enc.scale === 'zero' ? { zero: true } : {}),
+    };
+
+    encoding[enc.channel] = {
+      type: enc.type,
+      ...(enc.aggregate ? { aggregate: enc.aggregate } : {}),
+      ...(enc.field ? { field: enc.field } : {}),
+      ...(enc.stack ? { stack: enc.stack } : {}),
+      ...(enc.bin ? { bin: true } : {}),
+      ...scale,
+    }
+  }
+
+  const spec = {
+    mark,
+    encoding,
+  } as TopLevelUnitSpec;
+
+  return spec;
+}
---
+++ js/src/index.ts
@@ -0,0 +1,1 @@
+export * from './asp2vl';
