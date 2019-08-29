5a14771e3c33d5a21f333c30dd33ce543671ed14
Halden Lin <halden.lin@gmail.com>
2019-08-29 12:51:33 -0700

    Fix imports.

--- pair-generation/vl2cql.js
+++ pair-generation/vl2cql.js
@@ -1,13 +1,12 @@
-const cql = require("../../compassql/src");
-const spec = require("../../compassql/src/spec");
+const cql = require("compassql");
 
-export default function vl2cql(vlSpec) {
+module.exports = function vl2cql(vlSpec) {
   const data = vlSpec.data.values;
 
   const schema = cql.schema.build(data);
-  const specQ = spec.fromSpec(vlSpec);
+  const specQ = cql.fromSpec(vlSpec);
 
   const query = cql.model.SpecQueryModel.build(specQ, schema, {});
 
   return query;
-}
+};
